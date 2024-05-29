3669f1e9f68d65184a16ae8f9cb827e9bd1b6bad
maczi01 <mateusz.w.twardy@gmail.com>
2024-05-30 00:10:41 +0200

    chore: added global exception

--- src/main/java/com/codeart/bookaro/catalog/web/CatalogController.java
+++ src/main/java/com/codeart/bookaro/catalog/web/CatalogController.java
@@ -2,17 +2,22 @@ package com.codeart.bookaro.catalog.web;
 
 import com.codeart.bookaro.catalog.application.port.CatalogUseCase;
 import com.codeart.bookaro.catalog.domain.Book;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.math.BigDecimal;
 import java.net.URI;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
@@ -25,11 +30,10 @@ public class CatalogController {
     public List<Book> getAll(
             @RequestParam Optional<String> title,
             @RequestParam Optional<String> author
-            ) {
-        if (title.isPresent() && author.isEmpty()){
+    ) {
+        if (title.isPresent() && author.isEmpty()) {
             return catalogService.findByTitle(title.get());
-        }
-        else if (title.isEmpty() && author.isPresent()){
+        } else if (title.isEmpty() && author.isPresent()) {
             return catalogService.findByAuthor(author.get());
         }
         return catalogService.findAll();
@@ -49,7 +53,7 @@ public class CatalogController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Void> addBook(@RequestBody RestCreateBookCommand command){
+    public ResponseEntity<Void> addBook(@Valid @RequestBody RestCreateBookCommand command) {
         Book book = catalogService.addBook(command.toCommand());
         System.out.println("book----->" + book);
         URI uri = getUri(book);
@@ -58,7 +62,7 @@ public class CatalogController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteById(@PathVariable Long id){
+    public void deleteById(@PathVariable Long id) {
         catalogService.removeById(id);
     }
 
@@ -67,16 +71,25 @@ public class CatalogController {
         return ServletUriComponentsBuilder.fromCurrentRequestUri().path("/" + book.getId().toString()).build().toUri();
     }
 
+
     @Data
     private static class RestCreateBookCommand {
-            private String title;
-            private String author;
-            private Integer year;
-            private BigDecimal price;
+        @NotBlank
+        private String title;
 
-            CatalogUseCase.CreateBookCommand toCommand(){
-                return new CatalogUseCase.CreateBookCommand(title, author, year, price);
-            }
+        @NotBlank
+        private String author;
+
+        @NotNull
+        private Integer year;
+
+        @NotNull
+        @DecimalMin("0.00")
+        private BigDecimal price;
+
+        CatalogUseCase.CreateBookCommand toCommand() {
+            return new CatalogUseCase.CreateBookCommand(title, author, year, price);
+        }
     }
 
 
